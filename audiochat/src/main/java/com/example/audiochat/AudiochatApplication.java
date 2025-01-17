be9a741c7e54674a1eb9b380cfefd487b2889b5c
slhkrkya <slhkrkyh@gmail.com>
2025-01-17 18:31:54 +0300

    audiochat_test_backend

---
+++ audiochat/src/main/java/com/example/audiochat/AudiochatApplication.java
@@ -0,0 +1,13 @@
+package com.example.audiochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiochatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiochatApplication.class, args);
+	}
+
+}
---
+++ audiochat/src/test/java/com/example/audiochat/AudiochatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audiochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiochatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
