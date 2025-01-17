cafd87c8e9144d9e7db37b0cbc6ed77abf20278c
slhkrkya <slhkrkyh@gmail.com>
2025-01-17 21:53:24 +0300

    voicechatörnek

--- audiochat/src/main/java/com/example/audiochat/config/WebSocketConfig.java
+++ audiochat/src/main/java/com/example/audiochat/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/audio-chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/audio-chat").setAllowedOrigins("*");
 
     }
 }
