23c8a826e85d6abf5da00dd22c42491dfb53d779
slhkrkya <slhkrkyh@gmail.com>
2025-02-12 20:09:24 +0300

    WebSocket mesajlaşma
    Spring Boot ve WebSocket (STOMP) kullanarak gerçek zamanlı mesajlaşma sistemi

--- audiochat/src/main/java/com/example/audiochat/config/WebSocketConfig.java
+++ audiochat/src/main/java/com/example/audiochat/config/WebSocketConfig.java
@@ -9,14 +9,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/audio-chat").setAllowedOrigins("*");
+        registry.addEndpoint("/audio-chat").setAllowedOriginPatterns("*").withSockJS();
+    }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // Mesajların yayınlanacağı kanal
+        registry.setApplicationDestinationPrefixes("/app"); // Mesaj gönderme prefix'i
     }
 }
